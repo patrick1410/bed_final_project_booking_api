9aca29141cd49a2298b272e6d2825c7671dabb4c
Patrick <patrick_btc_96@hotmail.com>
2024-08-14 16:41:07 +0200

    made map/file structure and index.js template for order.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
+// IMPORTS
 import express from "express";
 
 const app = express();
+
+// SENTRY INIT
+
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN,
+//   integrations: [nodeProfilingIntegration()],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+//   // Set sampling rate for profiling - this is relative to tracesSampleRate
+//   profilesSampleRate: 1.0,
+// });
+
+// app.use(express.json());
+
+// LOGGER
+
+// ROUTES
+
+// SENTRY ERROR HANDLER?
+
+// ERROR HANDLER
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
