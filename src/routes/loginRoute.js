5c05985a3083f57864d82cfe3edf176dea080082
Patrick <patrick_btc_96@hotmail.com>
2024-08-22 14:12:36 +0200

    added a check to login.js so fields can't be empty!

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { login } from "../services/auth/login.js";
+import BadRequestError from "../errors/badRequestError.js";
 
 const router = Router();
 
 router.post("/", async (req, res, next) => {
   try {
     const { username, password } = req.body;
+
+    if (!username || !password) {
+      throw new BadRequestError("Username and password are required!"); // Maybe change this later
+    }
+
     const token = await login(username, password);
 
     if (!token) {
